0d4e320ddb2b66e24f500b40249041fb51292d2a
donovan <donoriviere@gmail.com>
2023-04-05 15:53:53 +0200

    set project

---
+++ client/52_gladiators/src/App.jsx
@@ -0,0 +1,8 @@
+import react, { useState } from 'react';
+import './index.css';
+
+function App() {
+  return <h1>jeux du cirque - 52</h1>;
+}
+
+export default App;
---
+++ serveur/app.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+require('express-async-errors');
+const cors = require('cors');
+
+const express = require('express');
+const app = express();
+
+// const authenticateUser = require('./middleware/authentication.js');
+
+//* routers
+// const authRouter = require('./routes/auth.js');
+// const tasksRouter = require('./routes/tasks.js');
+
+// const notFound = require('./middleware/notFound.js');
+// const errorHandler = require('./middleware/error-handler.js');
+
+app.use(cors());
+app.use(express.json());
+
+//* routes
+// app.use('/api/v1/auth', authRouter);
+// app.use('/api/v1/tasks', authenticateUser, tasksRouter);
+
+//*middleware
+// app.use(notFound);
+// app.use(errorHandler);
+
+app.listen(5000, () => {
+  console.log('Le serveur écoute sur le port 5000...');
+});
